860c0d20929caae5200ab38fa53ab80696f26c4e
Gyu-Yeon <idsopy@nate.com>
2022-04-14 19:23:32 +0900

    type day2

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-var 이름1 = "kim";
-var 이름2 = 123;
+//type 의 종류 string number boolean null undifined
+// let 이름: string = "kim";
+// let 나이: number = 50;
+// let 결혼했니: boolean = true;
+// let 회원들1: string[] = ["kim", "park"];
+// let 회원들2: { member1: string; member2: string } = {
+//   member1: "kim",
+//   member2: "park",
+// };
+var 이름 = "Choi";
+var 나이 = 25;
+var 출생지역 = "Seoul";
+var favorite = { 가수: "빅뱅", 제목: "거짓말" };
+var project = {
+    member: ["kim", "park"],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-let 이름1: string = "kim";
+//type 의 종류 string number boolean null undifined
 
-let 이름2: string | number = 123;
+// let 이름: string = "kim";
+// let 나이: number = 50;
+// let 결혼했니: boolean = true;
+
+// let 회원들1: string[] = ["kim", "park"];
+// let 회원들2: { member1: string; member2: string } = {
+//   member1: "kim",
+//   member2: "park",
+// };
+
+let 이름: string = "Choi";
+let 나이: number = 25;
+let 출생지역: string = "Seoul";
+
+let favorite: { 가수: string; 제목: string } = { 가수: "빅뱅", 제목: "거짓말" };
+
+let project: {
+  member: string[];
+  days: number;
+  started: boolean;
+} = {
+  member: ["kim", "park"],
+  days: 30,
+  started: true,
+};
